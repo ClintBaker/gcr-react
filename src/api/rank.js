eab9564d27e9b0498d9e8c29f8fb3bfd225fec79
Clint Baker <clintjbaker@gmail.com>
2024-01-25 14:28:56 -0700

    Level 5 complete - vite.config proxy for localhost:9000

--- src/api/rank.js
+++ src/api/rank.js
@@ -1,55 +1,55 @@
-import axios from "axios";
+import axios from 'axios'
 
-const url = "http://localhost:9000";
+const url = '/rank'
 
 export const getRanks = async (setRanks) => {
-  const ranks = await axios.get(`${url}/rank`);
+  const ranks = await axios.get(`${url}`)
 
   if (ranks.status === 200) {
-    setRanks(ranks.data.data);
+    setRanks(ranks.data.data)
   } else {
-    alert("Unable to get ranks");
+    alert('Unable to get ranks')
   }
-};
+}
 
 export const getRankById = async (setRank, setFormData, id) => {
-  const rank = await axios.get(`${url}/rank/${id}`);
+  const rank = await axios.get(`${url}/${id}`)
 
   if (rank.status === 200) {
-    setRank(rank.data.data);
-    setFormData(rank.data.data);
+    setRank(rank.data.data)
+    setFormData(rank.data.data)
   } else {
-    alert("Unable to get rank");
-    setRank(undefined);
+    alert('Unable to get rank')
+    setRank(undefined)
   }
-};
+}
 
 export const deleteRank = async (navigate, id) => {
-  const rank = await axios.delete(`${url}/rank/${id}`);
+  const rank = await axios.delete(`${url}/${id}`)
 
   if (rank.status === 200) {
-    navigate("/");
+    navigate('/')
   } else {
-    alert("Unable to delete rank");
+    alert('Unable to delete rank')
   }
-};
+}
 
 export const editRank = async (setRank, id, formData, setFormData) => {
-  const updatedRank = await axios.put(`${url}/rank/${id}`, formData);
+  const updatedRank = await axios.put(`${url}/${id}`, formData)
 
   if (updatedRank.status === 200) {
-    setRank(updatedRank.data.data);
+    setRank(updatedRank.data.data)
   } else {
-    alert("Unable to update rank");
+    alert('Unable to update rank')
   }
-};
+}
 
 export const createRank = async (navigate, formData) => {
-  const rank = await axios.post(`${url}/rank`, formData);
+  const rank = await axios.post(`${url}`, formData)
 
   if (rank.status === 200) {
-    navigate("/");
+    navigate('/')
   } else {
-    alert("Unable to create rank");
+    alert('Unable to create rank')
   }
-};
+}
